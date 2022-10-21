23d558cad390cde1db542483560272e2e985f69b
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-10-21 17:56:56 -0600

    updated css and API call

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+const scoresApi = 'https://api.sportsdata.io/v3/nfl/';
+const apiKey = '?key=a5e2eba777e4407396d813ee1fa5ef6d';
+const news = 'scores/json/News';
+// news api
+const getData = function () {
+  const scores = scoresApi + news + apiKey;
+  fetch(scores)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
+getData();
