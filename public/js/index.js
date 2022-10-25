3f293dcf3c3b8a62de5f45e015bd3bbff5bba227
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-10-24 22:21:33 -0600

    added API call

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,20 @@
 const rootApi = 'https://api.sportsdata.io/v3/nfl/';
 const apiKey = '?key=a5e2eba777e4407396d813ee1fa5ef6d';
 const news = 'scores/json/News';
-const teamName = ''; //id of the targeted element chosen
-// news api
+const season = 'scores/json/TeamSeasonStats/';
+const seasonYear = '2022';
+let newTeam = '';
+document.querySelectorAll('.logo').forEach((item) => {
+  item.addEventListener('click', (event) => {
+    const team = item.id;
+    console.log('team ' + team);
+    newTeam = team;
+    console.log('new Team ' + newTeam);
+    getSeason();
+  });
+});
+
+// news api0
 const getData = function () {
   const scores = rootApi + news + apiKey;
   fetch(scores)
@@ -13,3 +25,17 @@ getData();
 // stick with news feed
 // team stats by season (passing yards,rushing yards, touchdowns, fieldgoalsmade)
 // team standings
+
+const getSeason = function () {
+  const newSeason = rootApi + season + seasonYear + apiKey;
+  fetch(newSeason)
+    .then((response) => response.json())
+    // .then((data) => console.log(Object.keys(data)));
+    .then((data) => {
+      // need filter method
+      const teamData = data.filter((item) => item.Team === newTeam);
+      console.log(teamData);
+    });
+};
+
+// getSeason();
