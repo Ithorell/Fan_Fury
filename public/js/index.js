e8241bdfbd24c21d1ecf1cc2fef4fa945c75107e
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-10-24 19:34:23 -0600

    designed profile page, and moved over to profile.handlebars

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const rootApi = 'https://api.sportsdata.io/v3/nfl/';
+const scoresApi = 'https://api.sportsdata.io/v3/nfl/';
 const apiKey = '?key=a5e2eba777e4407396d813ee1fa5ef6d';
 const news = 'scores/json/News';
 // news api
@@ -9,3 +9,6 @@ const getData = function () {
     .then((data) => console.log(data));
 };
 getData();
+// stick with news feed
+// team stats by season (passing yards,rushing yards, touchdowns, fieldgoalsmade)
+// team standings
