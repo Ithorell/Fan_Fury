79c8a5bd3652e621dde5ba370dd781bb9675736e
Artemis <isaac.thorell@outlook.com>
2022-10-26 19:14:16 -0600

    schema Update

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ CREATE DATABASE users_db;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    password VARCHAR (255) NOT NULL,
+    PRIMARY KEY SELECT id,
 );
 
